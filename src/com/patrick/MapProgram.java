0c99b9ed363193e972ef967f59594d89698be474
patrickatcw <pj8392@yahoo.com>
2018-05-05 14:16:37 -0400

    just some experimenting

--- src/com/patrick/MapProgram.java
+++ src/com/patrick/MapProgram.java
@@ -12,6 +12,15 @@ public class MapProgram {
         //step 3 create map object
         //<String, String> = key, value
         Map<String, String> languages = new HashMap<>();
+        //step 7
+        if(languages.containsKey("Java")) {
+            System.out.println("Java already exists");
+        }else {
+            languages.put("Java", "a compiled high level, object-orientated, " +
+                    "platform independent language");
+            System.out.println("Java added successfully");
+            System.out.println("#########################################");
+        }
         languages.put("Java", "a compiled high level, object-orientated, " +
                 "platform independent language");
         languages.put("Python", "an interpreted, object-oriented, high-level programming " +
@@ -25,6 +34,32 @@ public class MapProgram {
         /*result; note called value by key
         a compiled high level, object-orientated, platform independent language
          */
+
+        //step 5 demonstrate value being overwritten, used key to call new messsage
+        languages.put("Java", "this is a Java course!");
+        System.out.println(languages.get("Java"));
+        //run
+        /*
+        this is a Java course!
+         */
+
+        //step 6
+        if(languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        }else{
+            languages.put("Java", "this is a Java course");
+            //Java is already in the map
+            //note 2nd line is not overwritten
+            //add step 7 above to illustrate, then run
+            /*
+            Java added successfully
+            #########################################
+            a compiled high level, object-orientated, platform independent language
+            this is a Java course!
+            Java is already in the map
+             */
+
+        }
 
     }
 
