f4962e0353d3f9561425a0275e5b0a91c4054c0e
patrickatcw <pj8392@yahoo.com>
2018-05-05 14:10:48 -0400

    demo of map object with hashmap

--- src/com/patrick/MapProgram.java
+++ src/com/patrick/MapProgram.java
@@ -1,8 +1,30 @@
 package com.patrick;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//step 1 this class
 public class MapProgram {
 
+    //step 2 run our code from this class
     public static void main(String[] args) {
+
+        //step 3 create map object
+        //<String, String> = key, value
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "a compiled high level, object-orientated, " +
+                "platform independent language");
+        languages.put("Python", "an interpreted, object-oriented, high-level programming " +
+                "language with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code");
+        languages.put("Lisp", "Thererin lies MADNESS!!");
+
+        //step 4 run
+        System.out.println(languages.get("Java"));
+        /*result; note called value by key
+        a compiled high level, object-orientated, platform independent language
+         */
 
     }
 
